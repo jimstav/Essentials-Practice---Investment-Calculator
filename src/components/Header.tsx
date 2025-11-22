bb3710a7340eb49d45ba9b72d83bfd2e68ec76d2
Dimitris Stavroulakis <jimstav@gmail.com>
2025-11-22 20:02:58 +0200

    add logo as a module

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
+import React from 'react';
+import logo from '../assets/investment-calculator-logo.png';
+
 const Header = () => {
   return (
     <header id="header">
-      <img
-        src="investment-calculator-logo.png"
-        alt="investment-calculator-logo"
-      />
+      <img src={logo} alt="Logo showing a money bag" />
       <h1>Investment Calculator</h1>
     </header>
   );
