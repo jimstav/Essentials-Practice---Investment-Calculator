26864e325d80df8981cced7047be9f96ba6c53ce
Dimitris Stavroulakis <jimstav@gmail.com>
2025-11-22 16:58:16 +0200

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header id="header">
+      <img
+        src="investment-calculator-logo.png"
+        alt="investment-calculator-logo"
+      />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
+
+export default Header;
